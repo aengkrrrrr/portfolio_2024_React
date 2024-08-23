447bfb12f8048b45f0041009162eec23fc53e876
songrim <srimm3399@naver.com>
2024-08-23 16:43:42 +0900

    css수정

--- src/Pro02.js
+++ src/Pro02.js
@@ -6,25 +6,25 @@ import circle from './images/circle_kang.png';
 function Pro02(){
   return (
     <>
-    <section class="project02 pr_grid">
-      <div class="content_wrap df aic">
-        <div class="project_ct_">
-          <h4 class="h4 mb-5">LMS 학습 쇼핑몰 - 사용자</h4>
-          <div class="tech_wrap df mb-3">
-            <em class="primary_btn">팀</em>
-            <em class="primary_btn">HTML</em>
-            <em class="primary_btn">CSS</em>
-            <em class="primary_btn">JAVACRIPT</em>
-            <em class="primary_btn">JQUERY</em>
+    <section className="project02 pr_grid">
+      <div className="content_wrap df aic">
+        <div className="project_ct_">
+          <h4 className="h4 mb-5">LMS 학습 쇼핑몰 - 사용자</h4>
+          <div className="tech_wrap df mb-3">
+            <em className="primary_btn">팀</em>
+            <em className="primary_btn">HTML</em>
+            <em className="primary_btn">CSS</em>
+            <em className="primary_btn">JAVACRIPT</em>
+            <em className="primary_btn">JQUERY</em>
           </div>
-          <div class="tech_wrap df">
-            <em class="primary_btn">PHP</em>
-            <em class="primary_btn">MYSQL</em>
+          <div className="tech_wrap df">
+            <em className="primary_btn">PHP</em>
+            <em className="primary_btn">MYSQL</em>
           </div>
           <p className="pj_p">빠르고 쉽게 배울 수 있다는 컨셉의<br/>
             딥러닝캥거루 LMS 학습 쇼핑몰 사이트의<br/>
             사용자 페이지입니다.</p>
-          <div class="link_wrap df aic">
+          <div className="link_wrap df aic">
             <a href="http://srimm3399.dothome.co.kr/clean_kangaroo/" target='_blank'><span class="material-symbols-outlined">open_in_new</span></a>
             <a href="https://github.com/aengkrrrrr/clean_kangaroo.git" target='_blank'><img src={gitImg} alt='깃허브 이미지' className="link_git" /></a>
           </div>
@@ -32,7 +32,7 @@ function Pro02(){
         <img src={proImg} alt='딥러닝캥거루 사용자' className="project_img" />
       </div>
       <img src={circle} alt='딥러닝캥거루 원모양' className="circle_img_r" />
-      <strong class="h2">Deep learning kangaroo</strong>
+      <strong className="h2">Deep learning kangaroo</strong>
     </section>
   </>
   )  
--- src/Pro05.js
+++ src/Pro05.js
@@ -6,28 +6,28 @@ import circle from './images/circle_lotte.png';
 function Pro05(){
   return (
     <>
-    <section class="project05 pr_grid">
-      <div class="content_wrap df aic">
+    <section className="project05 pr_grid">
+      <div className="content_wrap df aic">
       <img src={proImg} alt='롯데웰푸드 이미지' className="project_img" />
-        <div class="project_ct_r">
-          <h4 class="h4 mb-5">롯데웰푸드</h4>
-          <div class="tech_wrap df mb-3">
-            <em class="primary_btn">개인</em>
-            <em class="primary_btn">HTML</em>
-            <em class="primary_btn">CSS</em>
-            <em class="primary_btn">JAVACRIPT</em>
-            <em class="primary_btn">JQUERY</em>
+        <div className="project_ct_r">
+          <h4 className="h4 mb-5">롯데웰푸드</h4>
+          <div className="tech_wrap df mb-3">
+            <em className="primary_btn">개인</em>
+            <em className="primary_btn">HTML</em>
+            <em className="primary_btn">CSS</em>
+            <em className="primary_btn">JAVACRIPT</em>
+            <em className="primary_btn">JQUERY</em>
           </div>
           <p className="pj_p">시멘틱 태그를 활용하고 웹접근성에 준수하여,<br/>
           클론코딩한 PC반응형 사이트입니다.</p>
-          <div class="link_wrap df aic">
+          <div className="link_wrap df aic">
             <a href="http://srimm3399.dothome.co.kr/lottewelfood/index.html" target='_blank'><span class="material-symbols-outlined">open_in_new</span></a>
             <a href="" target='_blank'><img src={gitImg} alt='깃허브 이미지' className="link_git" /></a>
           </div>
        </div>
       </div>
       <img src={circle} alt='롯데웰푸드 원모양' className="circle_img_l" />
-      <strong class="h2">lotte wellfood</strong>
+      <strong className="h2">lotte wellfood</strong>
     </section>
   </>
   )  
